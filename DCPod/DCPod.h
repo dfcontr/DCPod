195186a8119bca1c9c75537ba79a001fbae413e3
Daniel Contreras <daniel.contreras@outlook.com>
2017-07-05 13:13:49 -0600

    Initial commit.

---
+++ DCPod/DCPod.h
@@ -0,0 +1,19 @@
+//
+//  DCPod.h
+//  DCPod
+//
+//  Created by Daniel Contreras on 7/5/17.
+//  Copyright © 2017 DC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DCPod.
+FOUNDATION_EXPORT double DCPodVersionNumber;
+
+//! Project version string for DCPod.
+FOUNDATION_EXPORT const unsigned char DCPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DCPod/PublicHeader.h>
+
+
---
+++ DCPod/Utils.swift
@@ -0,0 +1,13 @@
+//
+//  Utils.swift
+//  DCPod
+//
+//  Created by Daniel Contreras on 7/5/17.
+//  Copyright © 2017 DC. All rights reserved.
+//
+
+import Foundation
+
+func sum(a:Int, b:Int) -> Int {
+    return a + b
+}
